5529dbf8c9884068c01142055e96d9127a7b7cb4
brisalda <84285004+brisalda@users.noreply.github.com>
2021-05-16 21:22:53 +0200

    Update shkodra.js

--- shkodra.js
+++ shkodra.js
@@ -48,13 +48,7 @@
       }
     });
 
-       /*window.addEventListener('scroll', () => {
-        const scrollable = document.documentElement.scrollHeight - window.innerHeight;
-        const scrolled = window.scrollY;
-        if(Math.ceil(scrolled) === scrollable) {
-            alert('You\'ve reached the bottom!')
-        }
-       });*/
+   
 
        //show modal
        const toggleModal = () => {
